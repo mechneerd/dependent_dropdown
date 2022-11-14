21d974c18daf6eb3030a3af36e739a356683bd92
prasanth <106507229+mechneerd@users.noreply.github.com>
2022-11-14 07:30:36 +0530

    Update state_display.php

--- state_display.php
+++ state_display.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
     <title>FORM FOR PRATICE</title>
@@ -14,7 +14,7 @@
 	<table  style="border:1px solid;width:100%;text-align:center;"  >
 	
 	<tr style="border:1px solid">
-	<th style="border:1px solid" >S.No</th>
+	<th style="border:1px solid">Country</th>
 	<th style="border:1px solid">State</th>
 	<th style="border:1px solid">Action</th>
 	
@@ -31,7 +31,7 @@ $servername="localhost";
 $username="root";
 $password="";
 $dbname="prasanthdb";
-GLOBAL $counter;
+
 $con = new mysqli($servername,$username,$password,$dbname);
 
 $sql = "SELECT * FROM state";
@@ -40,13 +40,17 @@ $result = $con->query($sql);
 
 
 if($result->num_rows>0){
-	
-	$counter;
-	while($rows=$result->fetch_assoc()){
-		echo '<tr><td>'.$counter.'</td>'.'<td>'.$rows['state_name'].'</td>'
-.'<td>'.'<a text-decoration: "none"; href="update_state.php?id='.$rows["state_id"].' ">'.'<button style="background-color: #4CAF50;">'.'Update'.'</button>'.'</a>'
-.'<a text-decoration: "none"; href="delete_state.php?id='.$rows["state_id"].' ">'.'<button style="background-color: #ff0000;margin-left:15px;">'.'delete'.'</button>'.'</a>'.'</td>'.'</tr>';		
-		$counter++;		
+		while($rows=$result->fetch_assoc()){
+
+			$statesql="SELECT * FROM country WHERE country_id=".$rows['country_id'];	
+			$res = $con->query($statesql);
+			$row=$res->fetch_assoc();
+			
+
+			echo '<tr>'.'<td>'.$row['country_name'].'</td>'.'<td>'.$rows['state_name'].'</td>'
+				.'<td>'.'<a text-decoration: "none"; href="update_state.php?id='.$rows["state_id"].' ">'.'<button style="background-color: #4CAF50;">'.'Update'.'</button>'.'</a>'
+				.'<a text-decoration: "none"; href="delete_state.php?id='.$rows["state_id"].' ">'.'<button style="background-color: #ff0000;margin-left:15px;">'.'delete'.'</button>'.'</a>'.'</td>'.'</tr>';		
+			
 		
 		}	
 }
